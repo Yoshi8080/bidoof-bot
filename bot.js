a9604f17209a53f752db8d22e184409c08f0a2a5
Yoshi8080 <38776094+Yoshi8080@users.noreply.github.com>
2018-04-27 01:39:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-let cdseconds = 5;
-const talkedRecently = new Set();
+//let cdseconds = 5;
+//const talkedRecently = new Set();
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
-if(!message.content.startsWith('doof')) return;
-  if(cooldown.has(message.author.id)){
-    message.delete();
-    return message.reply("You have to wait 5 seconds before command.")
-}
-});
+//if(!message.content.startsWith('doof')) return;
+//  if(cooldown.has(message.author.id)){
+//    message.delete();
+//    return message.reply("You have to wait 5 seconds before command.")
+//}
+//});
 
 client.on('message', message => {
 if(message.content === "ayy") {
