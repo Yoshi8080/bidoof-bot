120e4d7362cb0b7502d655e081c3866b5d459a10
Yoshi8080 <38776094+Yoshi8080@users.noreply.github.com>
2018-04-27 01:41:11 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,7 +17,7 @@ client.on('ready', () => {
 client.on('message', message => {
 if(message.content === "_pray") {
    const ayy = client.emojis.find("name", "Ran_Doof2");
-   message.reply(`:pray: ${ayy}`);
+   message.reply(`prayed to Rainbow Bidoof! :pray: ${ayy}`);
 }
 });
 
