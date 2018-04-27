2f002c5b8a9569ec0bca0bb16df41c89a4b44259
Yoshi8080 <38776094+Yoshi8080@users.noreply.github.com>
2018-04-27 01:08:57 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,16 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const talkedRecently = new Set();
 
 client.on('ready', () => {
     console.log('I am ready!');
 });
-
-if(command === '-pray') {
-const ayy = client.emojis.find("name", "Ran_Doof2");
-message.reply(message.mentions.members.first, `has prayed` ${ayy});
-}
-}
 
 if(message.content === "ayy") {
    const ayy = client.emojis.find("name", "Ran_Doof2");
