ef0561a2320ef3192f84039567c92342602dc305
Yoshi8080 <38776094+Yoshi8080@users.noreply.github.com>
2018-04-27 01:09:34 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,10 +5,11 @@ client.on('ready', () => {
     console.log('I am ready!');
 });
 
-if(message.content === "ayy") {
-   const ayy = client.emojis.find("name", "Ran_Doof2");
-   message.reply(`${ayy} LMAO`);
-}
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
