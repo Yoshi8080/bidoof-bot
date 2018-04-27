48725ae0a437e426c66fbbd4703dbd2e3bcb2d94
Yoshi8080 <38776094+Yoshi8080@users.noreply.github.com>
2018-04-27 02:28:47 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,9 +5,13 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
+  bot.user.setStatus('available') // Can be 'available', 'idle', 'dnd', or 'invisible'
+    bot.user.setPresence({
+        game: {
+            name: 'Test',
+            type: 0
+        }
 });
-
-client.user.setActivity('ROBLOX', { type: 'Playing Bidoof Sim' });
 
 client.on('message', message => {
 if(message.content === "?pray") {
