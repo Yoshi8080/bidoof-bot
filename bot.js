758a3142a8fad3e61497b34d94653709f9e54cef
Yoshi8080 <38776094+Yoshi8080@users.noreply.github.com>
2018-04-27 01:27:30 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+let cooldown = new Set();
+let cdseconds = 5;
+const talkedRecently = new Set();
 
 client.on('ready', () => {
     console.log('I am ready!');
@@ -8,9 +11,13 @@ client.on('ready', () => {
 client.on('message', message => {
     if (message.content === 'doof') {
    const ayy = client.emojis.find("name", "Ran_Doof2");
-   message.reply(`:pray:` ${ayy});
+   message.reply(`:pray:` ${ayy}`);
   	}
 });
+  if(cooldown.has(message.author.id)){
+    message.delete();
+    return message.reply("You have to wait 5 seconds before command.")
+}
 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
