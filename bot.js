d881eea580e17fb396b9d9ff4868e0a45354ed38
Yoshi8080 <38776094+Yoshi8080@users.noreply.github.com>
2018-04-27 02:35:43 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ const client = new Discord.Client();
 
 client.on('ready', () => {
     console.log('I am ready!');
-client.user.setPresence({ game: { name: 'with discord.js' }, status: 'idle' });
+client.user.setPresence({ game: { name: 'Test' ,type: 0 }, status: 'available' });
 });
 
 client.on('message', message => {
