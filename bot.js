e591c25e44ef2528ebdcb4c1b9d2337f14db37f3
Yoshi8080 <38776094+Yoshi8080@users.noreply.github.com>
2018-04-27 01:59:24 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
 client.user.setGame("Praying to rainbow Bidoof; Playing Bidoof Simulator");
- client.on('ready', () => {
-     console.log('I am ready!');
- });
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
 client.on('message', message => {
 if(message.content === "?pray") {
