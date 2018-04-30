6a07ab81a72411a90f8382adef6661a354a6c3a0
Yoshi8080 <38776094+Yoshi8080@users.noreply.github.com>
2018-04-30 10:42:22 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -15,24 +15,6 @@ message.reply(` has prayed to Rainbow Bidoof! :pray: ${ayy}`);
     }
    
 });
-
-
-    if (talkedRecently.has(msg.author.id)) {
-            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
-    } else {
-client.on('message', message => {
- if(message.content === "ping") {
- message.reply(`pong`);
-     }
-});
-        // Adds the user to the set so that they can't talk for a minute
-        talkedRecently.add(msg.author.id);
-        setTimeout(() => {
-          // Removes the user from the set after a minute
-          talkedRecently.delete(msg.author.id);
-        }, 60000);
-    }
-});
         
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
