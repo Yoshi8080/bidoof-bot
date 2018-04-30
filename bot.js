e0dc28d3f51b297302eed0fb068dc0d7f2537ce8
Yoshi8080 <38776094+Yoshi8080@users.noreply.github.com>
2018-04-30 10:43:35 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,6 +8,25 @@ client.on('ready', () => {
 client.user.setPresence({ game: { name: 'with Bidoofs' ,type: 0 }, status: 'available' });
 });
 
+client.on('ready', () => {
+ if (talkedRecently.has(msg.author.id)) {
+            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
+    } else {
+client.on('message', message => {
+ if(message.content === "ping") {
+ message.reply(`pong`);
+     }
+});
+        // Adds the user to the set so that they can't talk for a minute
+        talkedRecently.add(msg.author.id);
+        setTimeout(() => {
+          // Removes the user from the set after a minute
+          talkedRecently.delete(msg.author.id);
+        }, 60000);
+    }
+});
+});
+
 client.on('message', message => {
 if(message.content === "doof!pray") {
 const ayy = client.emojis.find("name", "Ran_Doof2");
