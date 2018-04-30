bd5a639e134426cdb67da40720924f1d9b0c8ef7
Yoshi8080 <38776094+Yoshi8080@users.noreply.github.com>
2018-04-30 10:59:54 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,23 +9,36 @@ client.user.setPresence({ game: { name: 'with Bidoofs' ,type: 0 }, status: 'avai
 });
 
 
-
     if (talkedRecently.has(msg.author.id)) {
             msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
     } else {
-client.on('message', message => {
-if(message.content === "doof!pray") {
-const ayy = client.emojis.find("name", "Ran_Doof2");
-message.reply(` has prayed to Rainbow Bidoof! :pray: ${ayy}`);
-});
+
+           // the user can type the command ... your command code goes here :)
+
+        // Adds the user to the set so that they can't talk for a minute
         talkedRecently.add(msg.author.id);
         setTimeout(() => {
           // Removes the user from the set after a minute
           talkedRecently.delete(msg.author.id);
         }, 60000);
     }
+
+ //   if (talkedRecently.has(msg.author.id)) {
+//            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
+ //   } else {
+//client.on('message', message => {
+//if(message.content === "doof!pray") {
+//const ayy = client.emojis.find("name", "Ran_Doof2");
+//message.reply(` has prayed to Rainbow Bidoof! :pray: ${ayy}`);
+//});
+ //       talkedRecently.add(msg.author.id);
+   //     setTimeout(() => {
+  //        // Removes the user from the set after a minute
+ //         talkedRecently.delete(msg.author.id);
+ //       }, 60000);
+  //  }
    
-});
+//});
         
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
