ae01c97749c60822592d528934096f0a4f5afe24
Yoshi8080 <38776094+Yoshi8080@users.noreply.github.com>
2018-04-30 12:44:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,19 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+ 
+ 
+ 
 client.on('ready', () => {
     console.log('I am ready!');
 client.user.setPresence({ game: { name: 'with Bidoofs' ,type: 0 }, status: 'available' });
 });
-
-
+ 
 client.on('message', message => {
 if(message.content === "doof!pray") {
 const ayy = client.emojis.find("name", "Ran_Doof2");
 message.reply(` has prayed to Rainbow Bidoof! :pray: ${ayy}`);
+     } 
 });
-
-   
-
-        
+ 
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
