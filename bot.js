dd3eb285d6a8f35bd07ddd00f3b2be690633613d
Yoshi8080 <38776094+Yoshi8080@users.noreply.github.com>
2018-04-30 09:41:07 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const talkedRecently = new Set();
 
 
 client.on('ready', () => {
@@ -17,6 +17,22 @@ message.reply(` has prayed to Rainbow Bidoof! :pray: ${ayy}`);
 });
 
 
+    if (talkedRecently.has(msg.author.id)) {
+            msg.channel.send("Wait 1 minute before getting typing this again. - " + msg.author);
+    } else {
+client.on('message', message => {
+ if(message.content === "ping") {
+ message.reply(`pong`);
+     }
 
+        // Adds the user to the set so that they can't talk for a minute
+        talkedRecently.add(msg.author.id);
+        setTimeout(() => {
+          // Removes the user from the set after a minute
+          talkedRecently.delete(msg.author.id);
+        }, 60000);
+    }
+});
+        
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
